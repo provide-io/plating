06060b9c30528708d1964c4c445ddb78a7db7149
Tim <code@tim.life>
2025-10-19 10:12:54 -0700

    🔼⚙️ [skip ci] auto-commit

---
+++ src/plating/compiler/__init__.py
@@ -0,0 +1,8 @@
+#
+# plating/compiler/__init__.py
+#
+"""Plating example compilation modules."""
+
+from plating.compiler.grouped import ExampleGroup, GroupedExampleCompiler
+
+__all__ = ["ExampleGroup", "GroupedExampleCompiler"]
